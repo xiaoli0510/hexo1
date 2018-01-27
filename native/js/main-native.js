029d03abed266c3b7f78fd51776a7793dfff1aa8
xiaoli0510 <xiaoli0510>
2018-01-27 19:27:08 +0800

    实现原生js渲染页面

--- native/js/main-native.js
+++ native/js/main-native.js
@@ -5,13 +5,13 @@
  */
 
 var native = {
-    init:function(){
-        
+    init: function () {
+
         this.rendPage();
 
         this.bindEvent("on");
     },
-    rendPage:function(){
+    rendPage: function () {
 
         this.rendHead();
 
@@ -25,56 +25,139 @@ var native = {
 
         this.rendFooter();
     },
-    bindEvent:function(method){
-        if(method == "on" || method == "off"){
-            
+    bindEvent: function (method) {
+        if (method == "on" || method == "off") {
+
         }
     },
-    rendHead:function(){
+    rendHead: function () {
 
     },
-    rendUserInfo:function(){
-
-    },
-    rendProjectInfo:function(){
-
-    },
-    rendBlogInfo:function(){
-
-    },
-    rendAsideInfo:function(){
-
-    },
-    rendFooter:function(){
-
-    },
-    rend:function(data){
-        this.get(data.dataSrc,function(res){
-            // console.log(res);
-            var html = template(data.tplId,JSON.parse(res));
-            document.getElementById(data.containerId).innerHTML = html;
-        },function(error){
+    rendUserInfo: function () {
+        this.get("../data/userInfo.json", function (res) {
+            var html = native.buildUserBox(JSON.parse(res));
+            document.getElementsByClassName("body-header")[0].innerHTML = html;
+        }, function (error) {
             console.log(error);
         });
     },
-    get: function (url, sf, ef, p) {  
-        this.ajax({  
-            url: url,  
-            async: true,  
-            method: 'GET',  
-            contentType: 'application/x-www-form-urlencoded',  
-            crossDomain: true,  
-            success: function (response, xml) {
-                if ('function' == typeof sf)  
-                    sf(response, p);  
-            },  
-            fail: function (status) {
-                if ('function' == typeof ef)  
-                    ef(status, p);  
-            }  
-        });  
+    rendProjectInfo: function () {
+        this.get("../data/projectInfo.json", function (res) {
+            var html = native.buildProjectBox(JSON.parse(res));
+            document.getElementsByClassName("main-project")[0].innerHTML = html;
+        }, function (error) {
+            console.log(error);
+        });
     },
-    ajax:function(options) {
+    rendBlogInfo: function () {
+        this.get("../data/blogInfo.json", function (res) {
+            var html = native.buildBlogBox(JSON.parse(res));
+            document.getElementsByClassName("main-blog")[0].innerHTML = html;
+        }, function (error) {
+            console.log(error);
+        });
+    },
+    rendAsideInfo: function () {
+        this.get("../data/asideInfo.json", function (res) {
+            var html = native.buildAsideBox(JSON.parse(res));
+            document.getElementsByClassName("body-aside")[0].innerHTML = html;
+        }, function (error) {
+            console.log(error);
+        });
+    },
+    rendFooter: function () {
+        this.get("../data/footerInfo.json", function (res) {
+            var html = native.buildFooterBox(JSON.parse(res));
+            document.getElementsByClassName("body-footer")[0].innerHTML = html;
+        }, function (error) {
+            console.log(error);
+        });
+    },
+    buildUserBox: function (data) {
+        var userPhotoHtml = "<img class='header-user_photo' src= '" + data.user_photo + "' alt='photo'/>";
+        var userDescHtml = "<div class='header-user_info'>" +
+            "<hgroup>" +
+            "<h1 class='header-user_name'>" + data.user_name + "</h1>" +
+            "<h5 class='header-user_desc'>" + data.user_desc + "</h5>" +
+            "</hgroup>" +
+            "</div>";
+        var userNavHtml = "<nav class='header-nav'><ul class='nav-ul'>";
+        for (var i = 0, len = data.user_nav.length; i < len; i++) {
+            userNavHtml += "<li class='nav-li'><a href='" + data.user_nav[i].link + "'>" + data.user_nav[i].name + "</a></li>";
+        }
+        userNavHtml += "</ul></nav>";
+        return userPhotoHtml + userDescHtml + userNavHtml;
+    },
+    buildAsideBox: function (data) {
+        var asideHtml = "<div class='aside-code'>" +
+            "<img class='aside-code-img' src='" + data.code_img + "'/>" +
+            "<p class='aside-code-lable'>" + data.lable + "</p>" +
+            "</div>"
+        return asideHtml;
+    },
+    buildProjectBox: function (data) {
+        var projectHtml = "<h2>开源项目</h2><ul>";
+        for (var i = 0, len = data.project_list.length; i < len; i++) {
+            projectHtml += "<li>" +
+                "<section>" +
+                "<a href='" + data.project_list[i].link + "'>" +
+                "<h2 class='overflow_omit'>" + data.project_list[i].title + "</h2>" +
+                "<div class='overflow_omit'>" + data.project_list[i].desc + "</div>" +
+                "</a>" +
+                "</section>" +
+                "</li>"
+        }
+        projectHtml += "</ul>";
+        return projectHtml;
+    },
+    buildBlogBox: function (data) {
+        var blogHtml = "<h2>博客文章</h2><ul>";
+        for (var i = 0, len = data.article_list.length; i < len; i++) {
+            blogHtml += "<li>" +
+                "<section>" +
+                "<a href='" + data.article_list[i].link + "'>" +
+                "<h2 class='overflow_omit'>" + data.article_list[i].title + "</h2>" +
+                "<div class='overflow_omit'>" + data.article_list[i].desc + " </div>" +
+                "</a>" +
+                "</section>" +
+                "</li>"
+        }
+        blogHtml += "</ul><h3 class='main-view_more'><a href='" + data.blog_address + "'>查看更多</a></h3>";
+        return blogHtml;
+    },
+    buildFooterBox: function (data) {
+        var friendLinksHtml = "<aside class='footer-aside_link'><a>友情链接：</a>";
+        for (var i = 0, len = data.friend_links.length; i < len; i++) {
+            friendLinksHtml += "<a href='" + data.friend_links[i].link + "'>" + data.friend_links[i].name + "</a>";
+            if (i != len - 1) {
+                friendLinksHtml += "<span>|</span>";
+            }
+        }
+        friendLinksHtml += "</aside>";
+        var copyrightHtml = "<footer><p class='footer-copyright'>Copyright &copy; " + data.copyright + "</p></footer>";
+        return friendLinksHtml + copyrightHtml;
+    },
+    rend: function (data) {
+
+    },
+    get: function (url, sf, ef, p) {
+        this.ajax({
+            url: url,
+            async: true,
+            method: 'GET',
+            contentType: 'application/x-www-form-urlencoded',
+            crossDomain: true,
+            success: function (response, xml) {
+                if ('function' == typeof sf)
+                    sf(response, p);
+            },
+            fail: function (status) {
+                if ('function' == typeof ef)
+                    ef(status, p);
+            }
+        });
+    },
+    ajax: function (options) {
         options = options || {};
         options.type = (options.type || "GET").toUpperCase();
         options.dataType = options.dataType || "json";
@@ -111,16 +194,16 @@ var native = {
         }
     },
     //格式化参数
-    formatParams:function (data) {
+    formatParams: function (data) {
         var arr = [];
         for (var name in data) {
             arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
         }
-        arr.push(("v=" + Math.random()).replace(".",""));
+        arr.push(("v=" + Math.random()).replace(".", ""));
         return arr.join("&");
     }
 }
 
-window.onload = function(){
+window.onload = function () {
     native.init();
 }
